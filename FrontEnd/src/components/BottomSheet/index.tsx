cec514252b0740c6b112af0f8913d1524dae86d3
이챙 <cycy8527@gmail.com>
2022-12-18 21:01:19 +0900

    #3 Feat: bottom-sheet
    - props에 1이 들어오면 컨텐츠 최대높이값
    - Sheet.Content에 children 들어오도록 구현
    - 버튼은 추후 변경

---
+++ FrontEnd/src/components/BottomSheet/index.tsx
@@ -0,0 +1,30 @@
+import Sheet from 'react-modal-sheet';
+import { useState } from 'react';
+
+interface  BottomSheetProps{
+    children?: React.ReactNode,
+    contentH?: number,
+}
+
+const BottomSheet = ({children,contentH}: BottomSheetProps) => {
+  const [isOpen, setOpen] = useState(false);
+  const sheetDetent = contentH ? 'full-height' : 'content-height'
+
+  return (
+    <>
+      {/*클릭 버튼 */}
+      <button onClick={() => setOpen(true)}>Open sheet</button>
+      {/*Sheet */}
+      <Sheet isOpen={isOpen} onClose={() => setOpen(false)} detent={sheetDetent}>
+        <Sheet.Container>
+          <Sheet.Header />
+          <Sheet.Content>{children}</Sheet.Content>
+        </Sheet.Container>
+
+        <Sheet.Backdrop />
+      </Sheet>
+    </>
+  );
+}
+
+export default BottomSheet
