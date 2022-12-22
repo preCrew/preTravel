c4034160756939dcc9fb90a56cb87bef72a4f020
이챙 <cycy8527@gmail.com>
2022-12-22 13:49:51 +0900

    #4 Update: basic font

--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -1,7 +1,9 @@
+const plugin = require('tailwindcss/plugin')
 const px0_10 = { ...Array.from(Array(11)).map((_, i) => `${i}px`) };
 const px0_100 = { ...Array.from(Array(101)).map((_, i) => `${i}px`) };
 const px0_200 = { ...Array.from(Array(201)).map((_, i) => `${i}px`) };
 const px0_3000 = { ...Array.from(Array(3001)).map((_, i) => `${i}px`) };
+const remCalculate = (px) =>  (px / 16) + 'rem';
 
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
@@ -15,7 +17,44 @@ module.exports = {
       // => @media (min-width: 1024px) { ... }
     },
     extend: {
-      fontSize: px0_100,
+      fontSize: {
+        h4: [remCalculate(20),{
+          lineHeight:remCalculate(24),
+          fontWeight:'400'
+        }],
+        h4Bold: [remCalculate(20),{
+          lineHeight:remCalculate(24),
+          fontWeight:'800'
+        }],        
+        h5: [remCalculate(16),{
+          lineHeight:remCalculate(24),
+          fontWeight:'400'
+        }],
+        h5Bold: [remCalculate(16),{
+          lineHeight:remCalculate(24),
+          fontWeight:'800'
+        }],        
+        body1: [remCalculate(14),remCalculate(20)],
+        body1Bold: [remCalculate(14),{
+          lineHeight:remCalculate(20),
+          fontWeight:'800'
+        }],
+        body2: [remCalculate(13),remCalculate(18)],
+        body2Bold: [remCalculate(13),{
+          lineHeight:remCalculate(18),
+          fontWeight:'800'
+        }],
+        body3: [remCalculate(12),remCalculate(18)],
+        body3Bold: [remCalculate(12),{
+          lineHeight:remCalculate(18),
+          fontWeight:'800'
+        }],
+        body4: [remCalculate(10),remCalculate(15)],
+        body4Bold: [remCalculate(10),{
+          lineHeight:remCalculate(14),
+          fontWeight:'800'
+        }],
+      },
       width: px0_3000,
       height: px0_3000,
       fontWeight: {
@@ -33,27 +72,32 @@ module.exports = {
         gray4: '#eeeeee',
         gray4: '#F2F5F9',
       },
-      gridTemplateColumns: {
-        'auto-fit': 'repeat(auto-fit, 318px)',
-      },
-      keyframes: {
-        'skeleton-gradient': {
-          '0%': {
-            'background-color': 'rgba(165, 165, 165, 0.7)',
-          },
-          '50%': {
-            'background-color': 'rgba(165, 165, 165, 0.99)',
-          },
-          '100%': {
-            'background-color': 'rgba(165, 165, 165, 0.7)',
-          },
-        },
-      },
-      animation: {
-        'skeleton-gradient': 'skeleton-gradient 1.35s infinite ease-in-out',
-      },
     },
   },
 
-  plugins: [require('@tailwindcss/line-clamp')],
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+      plugin(function({ addUtilities }){
+        const newUtilities = {
+            '.safe-top' : {
+                paddingTop: 'constant(safe-area-inset-top)',
+                paddingTop: 'env(safe-area-inset-top)'
+            },
+            '.safe-left' : {
+                paddingLeft: 'constant(safe-area-inset-left)',
+                paddingLeft: 'env(safe-area-inset-left)'
+            },
+            '.safe-right' : {
+                paddingRight: 'constant(safe-area-inset-right)',
+                paddingRight: 'env(safe-area-inset-right)'
+            },
+            '.safe-bottom' : {
+                paddingBottom: 'constant(safe-area-inset-bottom)',
+                paddingBottom: 'env(safe-area-inset-bottom)'
+            }
+        }
+
+        addUtilities( newUtilities );
+    })    
+  ],
 };
