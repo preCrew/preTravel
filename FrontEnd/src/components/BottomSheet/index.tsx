ae6604a5b6755e7ca8cee9f74f3377a6fa22bfcd
이챙 <cycy8527@gmail.com>
2022-12-23 12:25:25 +0900

    #4 Fix: drag 시 바텀시트 닫혀지지 않는 이슈

--- FrontEnd/src/components/BottomSheet/index.tsx
+++ FrontEnd/src/components/BottomSheet/index.tsx
@@ -4,18 +4,19 @@ import { useState } from 'react';
 interface  BottomSheetProps{
     children?: React.ReactNode,
     contentH?: number,
+    dragOn?: boolean,
 }
 
-const BottomSheet = ({children,contentH}: BottomSheetProps) => {
+const BottomSheet = ({children,contentH,dragOn}: BottomSheetProps) => {
   const [isOpen, setOpen] = useState(false);
   const sheetDetent = contentH ? 'full-height' : 'content-height'
 
   return (
     <>
       {/*클릭 버튼 */}
-      <button onClick={() => setOpen(true)}>Open sheet</button>
+      <button onClick={() => setOpen(true)}>Open sheet?</button>
       {/*Sheet */}
-      <Sheet isOpen={isOpen} onClose={() => setOpen(false)} detent={sheetDetent} disableDrag={true}>
+      <Sheet isOpen={isOpen} onClose={() => setOpen(false)} detent={sheetDetent} disableDrag={dragOn}>
         <Sheet.Container>
           <Sheet.Header />
           <Sheet.Content>{children}</Sheet.Content>
