f53d334fdd9f18ea0d58238b43bdd691a506cc9a
Doosies <song961003@gmail.com>
2022-12-24 17:02:08 +0900

    feat: 마커 추가/제거, 위치이동 완료 - addMarker 함수로 마커 추가 - removeMarker 함수로 마커 제거 - setNowLocation 함수로 위치 이동

--- FrontEnd/src/pages/index.tsx
+++ FrontEnd/src/pages/index.tsx
@@ -1,21 +1,35 @@
 // import MapContainer from '@src/hooks/useKakaoMap';
-import useKakaoMap from '@src/hooks/useKakaoMap';
+import useKakaoMap from '@src/components/common/Map/useKakaoMap';
 import { Routes, Route } from 'react-router-dom';
 
+let test = 33.452613;
+let id = 1;
+
 const App = () => {
-  const { Map, setNowLocation } = useKakaoMap();
-  // const Map = useKakaoMap();
-  // console.log(process.env.KAKAOMAP_API_KEY);
+  const { Map, setNowLocation, addMarker, removeMarker } = useKakaoMap();
+  const handleClickButton = () => {
+    addMarker({ lat: test, lng: 126.570738 }, '근린공원', id);
+    test -= 0.0005;
+    id++;
+  };
+  const handleClickButton2 = () => {
+    removeMarker(1);
+  };
+  const handleClickButton3 = () => {
+    // 서울시 강남구의 위치로 이동
+    setNowLocation({ lat: 37.566826, lng: 126.9786567 });
+    // setNowLocation()
+  };
   return (
     <>
       <div className="absolute">
         <div className="absolute">
           <Map />
         </div>
-        <div className="relative z-10">
-          <button onClick={() => setNowLocation(33.452613, 126.570888)}>
-            sflksdjfsldkfsj
-          </button>
+        <div className="relative z-10 flex flex-col">
+          <button onClick={handleClickButton}> 마커 추가하기 </button>
+          <button onClick={handleClickButton2}>id 1인 마커 제거하기</button>
+          <button onClick={handleClickButton3}>서울시로 위치 이동하기</button>
         </div>
       </div>
       {/* {useKakaoMap()} */}
