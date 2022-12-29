cbc9cd52905da6dd1698c156f7adc0bd48d4db23
이챙 <cycy8527@gmail.com>
2022-12-29 18:13:14 +0900

    #10 title style

---
+++ FrontEnd/src/components/scheduleDetail/Title.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Title = () => {
+    return (
+        <h2 className='flex justify-center items-center px-2 m-auto rounded bg-gray4 h-30 max-w-[35%]'>
+            <span className='truncate text-body2Bold'>😘 경주영행경주영행경주영행경주영행경주영행경주영행경주영행경주영행경주영행경주영행</span>   
+        </h2>
+    );
+};
+
+export default Title;
