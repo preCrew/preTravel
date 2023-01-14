1f3a8e75fda392c3295ac9d24271551545c04d7d
Doosies <song961003@gmail.com>
2023-01-14 19:12:45 +0900

    #4: 종류별 버튼 생성 완료

---
+++ FrontEnd/src/components/common/Button/LoginButton/index.tsx
@@ -0,0 +1,46 @@
+import Button from '..';
+import kakao from '@src/assets/svgs/kakao.svg';
+import naver from '@src/assets/svgs/naver.svg';
+
+interface LoginButtonProps {
+  where: 'kakao' | 'naver';
+  onClick?: () => void;
+}
+
+const LoginButton = ({ where, onClick }: LoginButtonProps) => {
+  const source = {
+    kakao: {
+      src: kakao,
+      text: '카카오 로그인',
+      bg: 'bg-[#FEE500]',
+      textColor: 'text-black',
+      imageSize: 'w-40',
+    },
+    naver: {
+      src: naver,
+      text: '네이버 로그인',
+      bg: 'bg-[#03C75A]',
+      textColor: 'text-white',
+      imageSize: 'w-50',
+    },
+  };
+  return (
+    <>
+      <Button
+        type="large"
+        onClick={onClick}
+        className={`${source[where].bg} ${source[where].textColor}`}
+      >
+        <div className="w-50 flex justify-center ">
+          <img
+            src={source[where].src}
+            className={source[where].imageSize}
+          />
+        </div>
+        <p>{source[where].text}</p>
+      </Button>
+    </>
+  );
+};
+
+export default LoginButton;
