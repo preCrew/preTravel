3b0fd0996d4a09c672637f6f633d4e5da5efb9f3
이챙 <cycy8527@gmail.com>
2023-03-05 15:37:11 +0900

    #39 add: animation
    - 현재위치

--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -110,6 +110,25 @@ module.exports = {
         green1: 'var(--green1)',
       },
       keyframes: {
+        pulseRing: {
+          '0%': {
+            transform: 'scale(.33)'
+          },
+          '80%, 100%': {
+            opacity: 0
+          }
+        },
+        pulseDot: {
+          '0%': {
+            transform: 'scale(.8)'
+          },
+          '50%': {
+            transform: 'scale(1)'
+          },
+          '100%': {
+            transform: 'scale(.8)'
+          }
+        },        
         down: {
           '0%': { opacity: 0 },
           '100%': { opacity: 1 },
@@ -128,6 +147,8 @@ module.exports = {
         down: 'down 1.0s ease-in-out',
         up: 'up 1.0s ease-in-out',
         skeletonData: 'skeletonData 1.5s infinite ',
+        'pulse-dot': 'pulseDot 1.25s cubic-bezier(0.455, 0.03, 0.515, 0.955) -0.4s infinite',
+        'pulse-ring': 'pulseRing 1.25s cubic-bezier(0.215, 0.61, 0.355, 1) infinite'        
       },
     },
   },
