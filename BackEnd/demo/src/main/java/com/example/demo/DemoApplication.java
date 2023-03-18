fe394bf91f531483a4c56fb6ec1437de660a187c
cococoshin <dnflwlq1333@gmail.com>
2023-03-18 22:10:26 +0900

    allow origin 수정

--- BackEnd/demo/src/main/java/com/example/demo/DemoApplication.java
+++ BackEnd/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -18,15 +18,16 @@ public class DemoApplication {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
-    
+
     @Bean
     public WebMvcConfigurer webMvcConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8080",
-                                "https://web-fronttest-ll32glc6adwo3.gksl2.cloudtype.app")
+                        // .allowedOrigins("http://localhost:8080", "http://127.0.0.1:5500",
+                        //         "https://web-fronttest-ll32glc6adwo3.gksl2.cloudtype.app")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST")
                         .allowCredentials(true)
                         .exposedHeaders("Set-Cookie")
@@ -34,5 +35,5 @@ public class DemoApplication {
                         .maxAge(3600);
             }
         };
-}
+    }
 }
