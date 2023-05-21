39cd6f22c9dcadd6812e5be3dedda2d960dbf979
Doosies <song961003@gmail.com>
2023-05-21 23:05:08 +0900

    modify: chang folder name

--- FrontEnd/src/pages/MyPlaceInSchedule/index.tsx
+++ FrontEnd/src/pages/MyPlaceInSchedule/index.tsx
@@ -4,11 +4,11 @@ import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 
 import useKakaoMap from '@src/components/common/Map/useKakaoMap';
 import TabSlide from '@src/components/common/TabSlide/TabSlide';
-import Button from '@src/components/scheduleDetail/BottomSheet/Button';
-import CalendarCon from '@src/components/scheduleDetail/BottomSheet/CalendarCon';
-import MyScheduleCon from '@src/components/scheduleDetail/BottomSheet/MyScheduleCon';
-import CalendarBtn from '@src/components/scheduleDetail/CalendarBtn';
-import Title from '@src/components/scheduleDetail/Title';
+import Button from '@src/components/ScheduleDetail/BottomSheet/Button';
+import CalendarCon from '@src/components/ScheduleDetail/BottomSheet/CalendarCon';
+import MyScheduleCon from '@src/components/ScheduleDetail/BottomSheet/MyScheduleCon';
+import CalendarBtn from '@src/components/ScheduleDetail/CalendarBtn';
+import Title from '@src/components/ScheduleDetail/Title';
 
 import useGetMyPlaceInSchedule from '@src/hooks/react-query/useGetMyPlaceInSchedule';
 
