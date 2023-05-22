60e8832db428325f3f5c33b2be81db6223c8233e
Doosies <song961003@gmail.com>
2023-05-22 09:41:16 +0900

    대소문자 구분 config

--- FrontEnd/src/components/ScheduleDetail/Title.tsx
+++ FrontEnd/src/components/ScheduleDetail/Title.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-
 interface TitleProps {
   title: string;
 }
 
 const Title = ({ title }: TitleProps) => {
   return (
-    <h2 className="flex justify-center items-center px-2 m-auto rounded bg-gray4 h-30 max-w-[35%]">
+    <h2 className="m-auto flex h-30 max-w-[35%] items-center justify-center rounded bg-gray4 px-2">
       <span className="truncate text-body2Bold">😘 {title}</span>
     </h2>
   );
