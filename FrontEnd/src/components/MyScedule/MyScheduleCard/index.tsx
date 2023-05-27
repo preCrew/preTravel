7d0d047c83b891ba503c5929025a111bbf51c467
Song_Minhyung <song961003@gmail.com>
2023-05-27 17:49:07 +0900

    merge

--- FrontEnd/src/components/MyScedule/MyScheduleCard/index.tsx
+++ FrontEnd/src/components/MyScedule/MyScheduleCard/index.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import cardListAtom from '@src/recoil/cardList/atom';
 import { useMemo, useState } from 'react';
 import { useRecoilState } from 'recoil';
@@ -50,16 +51,65 @@ const MyScheduleCard = ({
     }
   };
   console.log('var(--contentInner)');
+=======
+import { useMemo, useState } from 'react';
+
+import { MySchedule } from '@src/hooks/react-query/useGetMyScheduleQuery';
+import useCardListState from '@src/hooks/recoil/useCardListState';
+
+import CheckBox from '../../common/CheckBox';
+import { Card, SkeletonCard } from './style';
+import tw from 'twin.macro';
+
+interface MyScheduleCardProps extends MySchedule {
+  deleteMode?: boolean;
+  index?: number;
+  onClickCard?: (cardIdx: number, idx: number) => void;
+}
+
+const MyScheduleCard = ({
+  startDate,
+  endDate,
+  name,
+  idx: cardIdx,
+  city,
+  deleteMode,
+  index,
+  file,
+  onClickCard,
+}: MyScheduleCardProps) => {
+  const { changeCardState } = useCardListState();
+  const [isSeleted, setIsSeleted] = useState(false);
+
+  const handleClickCheckBox = () => {
+    setIsSeleted(!isSeleted);
+    changeCardState(cardIdx, isSeleted);
+  };
+  const handleClickCard = () => {
+    if (deleteMode) {
+      handleClickCheckBox();
+    } else {
+      onClickCard?.(cardIdx, index as number);
+    }
+  };
+
+>>>>>>> dev
   const generateNum = Math.floor(Math.random() * 4 + 1);
   const bgColor = ['#fedcf5', '#c7ecf4', '#d6ede5', '#7ca3de', '#ffcfdb'];
   const bgColorValue = useMemo(() => bgColor[generateNum], []);
 
+<<<<<<< HEAD
+=======
+  console.log(file.length);
+
+>>>>>>> dev
   return (
     <li
       className={Card.block}
       onClick={handleClickCard}
     >
       <div
+<<<<<<< HEAD
         className={Card.blockInner(imagePaths ? imagePaths[0] : '', index)}
         style={{ backgroundColor: bgColorValue }}
       >
@@ -72,10 +122,59 @@ const MyScheduleCard = ({
           <p className={Card.date}>{`${dateRange[0]}${
             dateRange[1] && `~${dateRange[1]}`
           }`}</p>
+=======
+        css={[
+          Card.blockInner('', cardIdx),
+          file.length
+            ? { backgroundImage: `url(${file[0]?.fileDir})` }
+            : { background: bgColorValue },
+          tw`bg-center bg-no-repeat bg-cover`,
+        ]}
+      >
+        {/* 삭제모드면 체크박스 표시 */}
+        {deleteMode && (
+          <CheckBox
+            onChange={handleClickCheckBox}
+            cheked={isSeleted}
+          />
+        )}
+        <div className={Card.content}>
+          {/* 언제나 카드 표시 */}
+          <p className={Card.title}>{name}</p>
+          <p className={Card.region}>{city}</p>
+          <p className={Card.date}>{`${startDate} ~ ${endDate}`}</p>
+>>>>>>> dev
         </div>
       </div>
     </li>
   );
 };
 
+<<<<<<< HEAD
+=======
+const iterator = [1, 2, 3, 4, 5, 6, 7, 8];
+export const SkeletonMyScheduleCard = () => {
+  return (
+    <>
+      <div css={tw`relative grid grid-cols-2 gap-4 w-h-full content-inner`}>
+        {iterator.map(id => (
+          <div
+            css={SkeletonCard.Inner}
+            key={id}
+          >
+            {/* 삭제모드면 체크박스 표시 */}
+            <div css={SkeletonCard.Content}>
+              {/* 언제나 카드 표시 */}
+              <p css={SkeletonCard.Title} />
+              <p css={SkeletonCard.Region} />
+              <p css={SkeletonCard.Date} />
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+>>>>>>> dev
 export default MyScheduleCard;
--- FrontEnd/src/pages/mySchedule/index.tsx
+++ FrontEnd/src/pages/mySchedule/index.tsx
@@ -1,93 +1,3 @@
-<<<<<<< HEAD
-import useKakaoMap from '@src/components/common/Map/useKakaoMap';
-import TabSlide from '@src/components/common/TabSlide/TabSlide';
-import Button from '@src/components/scheduleDetail/BottomSheet/Button';
-import CalendarCon from '@src/components/scheduleDetail/BottomSheet/CalendarCon';
-import MyScheduleCon from '@src/components/scheduleDetail/BottomSheet/MyScheduleCon';
-import CalendarBtn from '@src/components/scheduleDetail/CalendarBtn';
-import Title from '@src/components/scheduleDetail/Title';
-import { currentScheduleAtom, selectedDayAtom } from '@src/recoil/date/atom';
-import { calendarIsOpenAtom } from '@src/recoil/modal/atom';
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
-import { useCallback, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { useRecoilState, useRecoilValue } from 'recoil';
-// import Title from '@src/components/ScheduleDetail/Title';
-
-async function fetchPost(postId: number) {
-  try {
-    const response = await axios.get(`http://localhost:3001/posts/${postId}`);
-    return response.data;
-  } catch (err) {
-    if (err instanceof Error) {
-      console.log(err.message);
-    }
-  }
-}
-
-const MySchedule = () => {
-  const { id } = useParams();
-  const postId = (id as unknown as number) * 1;
-  const { data, isLoading, error, isError } = useQuery(['posts', postId], () =>
-    fetchPost(postId),
-  );
-  const [currentScheduleState, setCurrentScheduleState] =
-    useRecoilState(currentScheduleAtom);
-  const selectedDayState = useRecoilValue(selectedDayAtom);
-  const { Map, setNowLocation, drawOverlayOnMap } = useKakaoMap();
-  const calendarIsOpenState = useRecoilValue(calendarIsOpenAtom);
-  const scheduleDaysArr: number[] = Array(currentScheduleState.schedule.length)
-    .fill(null)
-    .map((_, i) => i + 1);
-
-  useEffect(() => {
-    setCurrentScheduleState(data);
-    //데이터 받아온후 지도 그리기
-    const introLocation = setTimeout(() => {
-      if (currentScheduleState) {
-        //setNowLocation({ lat: 33.505723800807067, lng: 126.491236832635933 });
-        drawOverlayOnMap(currentScheduleState.schedule[selectedDayState]?.list);
-      }
-    }, 0);
-    return () => clearTimeout(introLocation);
-  }, [
-    data,
-    currentScheduleState,
-    currentScheduleState.schedule[selectedDayState],
-  ]);
-
-  const onClickBack = () => {
-    console.log(1);
-  };
-
-  const onCallMap = useCallback(() => {
-    return <Map />;
-  }, [currentScheduleState]);
-
-  return (
-    <>
-      <header className="fixed left-0 right-0 top-5 z-10">
-        <Title title={currentScheduleState.title} />
-        <Button
-          onClick={onClickBack}
-          name="나가기"
-          style="absolute right-basic top-0"
-        />
-        <div className="relative mt-4 ml-basic">
-          <CalendarBtn />
-          {!calendarIsOpenState && (
-            <TabSlide
-              data={scheduleDaysArr}
-              category="일차별"
-            />
-          )}
-        </div>
-      </header>
-      {/*====지도====*/}
-      {onCallMap()}
-      {calendarIsOpenState ? <CalendarCon /> : <MyScheduleCon />}
-=======
 import { useNavigate } from 'react-router-dom';
 import { Suspense, useState } from 'react';
 import { useRecoilValue } from 'recoil';
@@ -168,13 +78,8 @@ const MySchedule2 = () => {
           </Button>
         )}
       </div>
->>>>>>> dev
     </>
   );
 };
 
-<<<<<<< HEAD
-export default MySchedule;
-=======
 export default MySchedule2;
->>>>>>> dev
