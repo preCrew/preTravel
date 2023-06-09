ca28db7df9065dd3459731795b59c6797733701a
이챙 <cycy8527@gmail.com>
2023-06-09 17:07:03 +0900

    #63 style: myshedule top

--- FrontEnd/src/pages/MySchedule/style.ts
+++ FrontEnd/src/pages/MySchedule/style.ts
@@ -2,10 +2,10 @@ const page = `
   flex flex-col items-center p-3 
 `;
 const childrenBox = `
-  flex w-full justify-between items-center 
+  flex w-full justify-between items-center px-5 pt-8 pb-4 
 `;
 const title = `
-  text-h4Bold p-7 select-none
+  text-h4Bold select-none
 `;
 const buttonBox = `
   flex gap-5
