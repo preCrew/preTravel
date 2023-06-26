ae7c452cb537d1bb3012a6280823fc88da55d4f3
이챙 <cycy8527@gmail.com>
2023-06-26 13:57:09 +0900

    #73 fix:메인 바텀시트 오류

--- FrontEnd/src/components/common/Main/MainModal/index.tsx
+++ FrontEnd/src/components/common/Main/MainModal/index.tsx
@@ -3,6 +3,7 @@ import { AiFillStar } from 'react-icons/ai';
 import Slider from 'react-slick';
 
 import BottomSheetWrap from '@src/components/ScheduleDetail/BottomSheet/BottomSheetWrap';
+import { useState } from 'react';
 
 type TMarkerData = {
   address: string;
@@ -22,31 +23,61 @@ interface MainModalProps {
 }
 
 const MainModal = ({ data }: MainModalProps) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const settings = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    beforeChange: (current: any, next: any) => setCurrentSlide(next),
+  };
+
   return (
-    <BottomSheetWrap drag={false}>
+    <BottomSheetWrap
+      drag={false}
+      snapIdx={2}
+    >
       {data.star ? (
-        <>
-          <Slider
-            slidesToShow={1}
-            lazyLoad="ondemand"
-          >
-            {data.file?.map(image => (
-              <div>{image}</div>
-            ))}
-          </Slider>
-          <h3 css={tw`text-body1Bold`}>{data.name}</h3>
-          <div css={tw`flex items-center text-body1`}>
-            <AiFillStar
-              color="#ffcc00"
-              css={tw`mr-1`}
-            />
-            {data.star}
+        <div css={tw`flex`}>
+          {data.file?.length && (
+            <div css={tw`w-[55%] mr-2 relative h-175 overflow-hidden`}>
+              <Slider
+                {...settings}
+                css={tw`w-[100%]`}
+                className="main-modal__slider"
+              >
+                {data.file?.map((image: any) => (
+                  <div css={tw``}>
+                    {/* <img src={image.fileDir} />{' '} */}
+                    <img
+                      src={image.fileDir}
+                      css={tw`object-cover w-full h-full`}
+                    />
+                  </div>
+                ))}
+              </Slider>
+              <span
+                css={tw`absolute bottom-1 right-1 z-10 text-white text-body4Bold p-1  before:(content-[''] -z-1 rounded absolute left-0 top-0 w-full h-full bg-black opacity-50)`}
+              >
+                {currentSlide + 1}/{data.file?.length}
+              </span>
+            </div>
+          )}
+          <div css={tw`w-[40%]`}>
+            <h3 css={tw`text-body1Bold line-clamp-2`}>{data.name}</h3>
+            <div css={tw`flex items-center text-body1`}>
+              <AiFillStar
+                color="#ffcc00"
+                css={tw`mr-1`}
+              />
+              {data.star}
+            </div>
+            <p css={tw`mt-4 text-body2`}>{data.address}</p>
           </div>
-          <p css={tw`mt-4 text-body2`}>{data.address}</p>
-        </>
+        </div>
       ) : (
         <>
-          <h3 css={tw`text-body1Bold`}>{data.name}</h3>
+          <h3 css={tw`text-body1Bold line-clamp-2`}>{data.name}</h3>
           <p css={tw`mt-4 text-body2`}>{data.address}</p>
         </>
       )}
