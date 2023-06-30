05b68a61913fbb620e89478d5ae0b6476f976283
이챙 <cycy8527@gmail.com>
2023-07-01 00:19:14 +0900

    #71 fix: 프로필이미지

--- FrontEnd/src/components/Mypage/Profile/index.tsx
+++ FrontEnd/src/components/Mypage/Profile/index.tsx
@@ -1,6 +1,7 @@
 import useGetUserInfo from '@src/hooks/react-query/useGetUserInfo';
-import React from 'react';
+import profile from '@src/assets/svgs/ico_profile.svg?url';
 import tw from 'twin.macro';
+import { css } from '@emotion/react';
 
 const Profile = () => {
   const {
@@ -11,7 +12,14 @@ const Profile = () => {
     <div
       css={tw`relative z-10 flex flex-col items-center justify-center text-white h-1/2`}
     >
-      <div css={tw`mb-8 bg-white rounded-full w-110 h-110`}></div>
+      <div
+        css={[
+          tw`mb-8 bg-white rounded-full w-110 h-110`,
+          css`
+            background: #fff url(${profile}) center center no-repeat;
+          `,
+        ]}
+      ></div>
       <h3 css={tw`text-h2Bold`}>{member.name}</h3>
       <p css={tw`text-body3`}>{member.email}</p>
     </div>
