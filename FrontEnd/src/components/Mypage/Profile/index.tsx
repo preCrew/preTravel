31c1549e65aaa1a271327411f5baa5b091bbfb51
이챙 <cycy8527@gmail.com>
2023-06-30 23:55:24 +0900

    #71 fix: 회원 아이디,이름

--- FrontEnd/src/components/Mypage/Profile/index.tsx
+++ FrontEnd/src/components/Mypage/Profile/index.tsx
@@ -1,14 +1,19 @@
+import useGetUserInfo from '@src/hooks/react-query/useGetUserInfo';
 import React from 'react';
 import tw from 'twin.macro';
 
 const Profile = () => {
+  const {
+    data: { member },
+  } = useGetUserInfo('1');
+
   return (
     <div
       css={tw`relative z-10 flex flex-col items-center justify-center text-white h-1/2`}
     >
       <div css={tw`mb-8 bg-white rounded-full w-110 h-110`}></div>
-      <h3 css={tw`text-h2Bold`}>이채영</h3>
-      <p css={tw`text-body3`}>dddd@naver.com</p>
+      <h3 css={tw`text-h2Bold`}>{member.name}</h3>
+      <p css={tw`text-body3`}>{member.email}</p>
     </div>
   );
 };
