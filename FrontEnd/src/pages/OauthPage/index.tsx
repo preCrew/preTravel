2005de36c6659f0c1bb5735804cf4e74bf864738
Song_Minhyung <song961003@gmail.com>
2023-07-16 14:20:51 +0900

    fix: oauthPage 작동방식 변경 - 데이터 받아올 때 까지 기다렸다가 -> 받아오면 확인

--- FrontEnd/src/pages/OauthPage/index.tsx
+++ FrontEnd/src/pages/OauthPage/index.tsx
@@ -35,61 +35,25 @@ const OauthPage = ({}: OauthPageProps) => {
   });
 
   useEffect(() => {
-    fetchUserMemeberIdx();
+    const getUserData = async () => {
+      await fetchUserMemeberIdx();
+
+      if (isSuccess) {
+        setUserState(state => ({
+          ...state,
+          accessToken: userMemberIdx.accessToken,
+          isLogin: true,
+          id: userMemberIdx.id,
+        }));
+        setCookies('userCookie', userMemberIdx.id, { path: '/' });
+        navigate('/');
+      } else if (isError) {
+        alert(isError);
+        navigate('/');
+      }
+    };
+    getUserData();
   }, []);
-
-  useEffect(() => {
-    if (isSuccess) {
-      setUserState(state => ({
-        ...state,
-        accessToken: userMemberIdx.accessToken,
-        isLogin: true,
-        id: userMemberIdx.id,
-      }));
-      setCookies('userCookie', userMemberIdx.id, { path: '/' });
-      navigate('/');
-    }
-  }, [isSuccess]);
-
-  useEffect(() => {
-    if (isError) {
-      alert(isError);
-      navigate('/');
-    }
-  }, [isError]);
-  // const get = async () => {
-  // const baseUrl = process.env.REAL_SERVER_URL; //getLoginUri('base');
-  // const where = params.where as 'kakao' | 'naver';
-  // // const href = new URLSearchParams(location.search);
-  // const code = getQueryString('code');
-  // const state = getQueryString('state');
-  // try {
-  //   const reqUrl = {
-  //     kakao: `${baseUrl}/oauth/${where}?code=${code}`,
-  //     naver: `${baseUrl}/oauth/${where}?code=${code}&state=${state}`,
-  //   };
-  //   const res = await axios.get(reqUrl[where], {
-  //     withCredentials: true,
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //   });
-  //   const data = res.data.data;
-  //   if (data) {
-  //     setUserState(state => ({
-  //       ...state,
-  //       accessToken: data.accessToken,
-  //       isLogin: true,
-  //     }));
-  //     navigate('/');
-  //   } else {
-  //     throw new Error('서버에서 데이터를 받아오지 못했습니다.');
-  //   }
-  // } catch (e) {
-  //   alert(`${e} \n로그인에 실패했습니다. \n이전 페이지로 돌아갑니다.`);
-  //   navigate(-1);
-  // }
-  // };
 
   return null;
 };
